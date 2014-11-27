2c084cd9f8d8503d5a05e7fb4ab5aebdab22d49e
juneyoung <juneyoung85@gmail.com>
2014-11-27 23:34:33 +0900

    arcTo problem

--- client_merge.sh
+++ client_merge.sh
@@ -4,14 +4,14 @@
 echo -e 'github auto client-remote merge\nver.0.1\n2014.11.23\nauthor : JuneyoungOh'
 
 #remove sharp mark of A1 if you need new remote repository
-#A1 - echo -e 'enter your remote address'
-#A1 - read remoteaddr
+echo -e 'enter your remote address'
+read remoteaddr
 
 echo -e 'enter commit message'
 read commitmsg
 
-# A1 - git remote rm origin
-# A1 - git remote add upstream $remoteaddr
+git remote rm origin
+git remote add upstream $remoteaddr
 
 git remote -v
 git fetch upstream
